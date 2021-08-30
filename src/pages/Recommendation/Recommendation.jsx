8057d2e8155ab3c1b109fff72cf5a23e2e0fcf77
LuanSSouza <luan.souza27@hotmail.com>
2021-08-29 23:13:28 -0300

    Implemmenting recommendation questionnaire

--- src/pages/Recommendation/Recommendation.jsx
+++ src/pages/Recommendation/Recommendation.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 // Bootstrap imports
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
 
 class Recommendation extends Component {
   state = {
@@ -17,6 +17,31 @@ class Recommendation extends Component {
         Seconds” contributes a great deal to the inspirational action scenes.
       `,
     },
+    details: 1,
+    understood: null,
+    convincing: null,
+    discover: null,
+    trust: null,
+  };
+
+  handleChangeDetails = (event) =>
+    this.setState({ details: event.target.value });
+
+  handleChangeUnderstood = (event) =>
+    this.setState({ details: event.target.understood });
+
+  handleChangeConvincing = (event) =>
+    this.setState({ details: event.target.convincing });
+
+  handleChangeDiscover = (event) =>
+    this.setState({ details: event.target.discover });
+
+  handleChangeTrust = (event) => this.setState({ details: event.target.trust });
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.history.push("/itens");
   };
 
   render() {
@@ -33,11 +58,103 @@ class Recommendation extends Component {
         <Row>
           <Col className="text-center">{this.state.item.explanation}</Col>
         </Row>
-        <Row>
-          <Col md={{ span: 4, offset: 8 }} className="text-right">
-            <Button variant="primary">Next</Button>
-          </Col>
-        </Row>
+
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="details">
+            <Form.Label>Level Of Deatils:</Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.details}
+              onChange={this.handleChangeDetails}
+            >
+              <option value="0">Short</option>
+              <option value="1">Medium</option>
+              <option value="2">Long</option>
+            </Form.Control>
+          </Form.Group>
+          <hr />
+          <h3>Questionnaire</h3>
+          <Form.Group controlId="understood">
+            <Form.Label>
+              I understood why this movie was recommended to me:
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.understood}
+              onChange={this.handleChangeUnderstood}
+            >
+              <option value="" disabled selected>
+                Select a option
+              </option>
+              <option value="0">Strongly disagree</option>
+              <option value="1">Disagree</option>
+              <option value="2">I don't know</option>
+              <option value="3">Strongly agree</option>
+              <option value="4">Agree</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="convincing">
+            <Form.Label>
+              The explanation made the recommendation more convinving:
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.convincing}
+              onChange={this.handleChangeConvincing}
+            >
+              <option value="" disabled selected>
+                Select a option
+              </option>
+              <option value="0">Strongly disagree</option>
+              <option value="1">Disagree</option>
+              <option value="2">I don't know</option>
+              <option value="3">Strongly agree</option>
+              <option value="4">Agree</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="discover">
+            <Form.Label>
+              The explanation helped me discover new information about this
+              movie:
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.discover}
+              onChange={this.handleChangeDiscover}
+            >
+              <option value="" disabled selected>
+                Select a option
+              </option>
+              <option value="0">Strongly disagree</option>
+              <option value="1">Disagree</option>
+              <option value="2">I don't know</option>
+              <option value="3">Strongly agree</option>
+              <option value="4">Agree</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="trust">
+            <Form.Label>
+              The explanation increased my trust in the recommender system:
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.trust}
+              onChange={this.handleChangeTrust}
+            >
+              <option value="" disabled selected>
+                Select a option
+              </option>
+              <option value="0">Strongly disagree</option>
+              <option value="1">Disagree</option>
+              <option value="2">I don't know</option>
+              <option value="3">Strongly agree</option>
+              <option value="4">Agree</option>
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Next
+          </Button>
+        </Form>
       </Container>
     );
   }
